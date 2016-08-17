9e9504b2a17b16906251c142e32c74ced1d09233
Raghukrishnan R <rkrsmail@gmail.com>
2016-08-17 23:31:51 +0530

    Splash screen added

---
+++ gramasira/src/main/java/rkr/binatestation/gramasira/SplashScreen.java
@@ -0,0 +1,16 @@
+package rkr.binatestation.gramasira;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(getBaseContext(), HomeActivity.class));
+        finish();
+    }
+}
